3a971591c78cda8966014fa78ca00d5360ccc02a
Kyaah <kyaahh@gmail.com>
2021-09-12 19:42:05 -0500

    commit

--- songshion/client/src/App.js
+++ songshion/client/src/App.js
@@ -1,10 +1,38 @@
-import UserInfo from './components/userinfo/UserInfo'
-function App() {
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import UserInfo from './components/userinfo/UserInfo';
+import Events from './components/events/Events';
+
+const App = () => {
   return (
-    <div className="App">
-      <UserInfo/>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <UserInfo />
+          </Route>
+
+          <Route path="/events" exact>
+            <Events />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <UserInfo/>
+//     </div>
+//   );
+// }
+
+// export default App;

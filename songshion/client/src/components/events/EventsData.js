7bb042ae2855bc0040da4cd94f2b5533bbae048f
sydneyw-cyber <sydneyw521@gmail.com>
2021-09-16 22:35:35 -0500

    getting data from ticketmaster api to render in console

--- songshion/client/src/components/events/EventsData.js
+++ songshion/client/src/components/events/EventsData.js
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import Moment from 'moment';
 export default function App() {
   const [search, setSearch] = useState('');
   const [list, setList] = useState([]);
+  const [fetchdata, setFetchData] = useState([])
+
   async function handleClick(e) {
     e.preventDefault();
-    console.log('click', search);
+
+    const date = Moment().format()
+    const startDate = date.slice(0, 19)
+    const endDate = Number(date.slice(8, 10))+3
+    const firstHalf = date.slice(0, 8)
+    const secondHalf = date.slice(10, 19)
+    console.log("hello" , secondHalf);
+    const realEndDate = firstHalf + endDate.toString()+secondHalf;
+    console.log("realEndDate" , realEndDate);
+    console.log(date)
+    
+    console.log(Number(date.slice(8, 10))+3)
+
     try {
       const response = await fetch(
-        'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=Chicago&localStartEndDateTime=2021-10-05T00:00:00,2021-10-06T00:00:00&apikey=wCk6GSsb161G6CrEc2a8Rv2BQAEZ7PdQ',
+       // 2021-09-16T21:30:36-05:00
+        'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city='+search+'&localStartEndDateTime='+startDate+','+realEndDate+'&apikey=wCk6GSsb161G6CrEc2a8Rv2BQAEZ7PdQ',
         {
           method: 'GET',
         }
       );
       const fetchedQuery = await response.json();
-      setList(fetchedQuery);
+      setFetchData(fetchedQuery._embedded.events);
     // line below works in pulling in artist name  
-      console.log('Artist Name:', fetchedQuery._embedded.events[0].name);
+      // console.log('Artist Name:', fetchedQuery._embedded.events[0].name);
     // lines below are to try to store the artist name 
     //   setList(fetchedQuery._embedded.events[0].name);
     //   console.log('testingsetArtist:', setList);
@@ -23,6 +39,22 @@ export default function App() {
       console.log(err);
     }
   }
+  useEffect(() =>{
+    console.log('Cars', fetchdata[0].name)
+  },[fetchdata]);
+//   let musicData = fetchdata._embedded.events.map((event)=>{
+//    if (musicData.length == 0){
+//      return (<div></div>)
+//    } else {
+//    return(
+//      <div>
+//        <p>YO</p>
+//      </div>
+//    )
+//    }
+//  })
+
+
   const handleInput = e => {
     setSearch(e.target.value);
   };
@@ -40,8 +72,9 @@ export default function App() {
           placeholder="search for giphy"
           onChange={handleInput}
         />
-        <input type="button" value="SEARCH" onClick={handleClick} />
+        <input type="button" onClick={handleClick} />
       </form>
+      {/* {musicData} */}
     </div>
   );
 }

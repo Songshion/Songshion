d9b67f8e391c71593bbee19a73c880fbd613d85b
tejeshpatel <tejeshpatel@live.com>
2021-09-18 00:57:06 -0500

    adding css updates

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import EventsData from '../events/EventsData';
+
+function Footer() {
+    return (
+      <footer class="footer">
+          <p></p>
+    </footer>
+    );
+  }
+export default Footer;

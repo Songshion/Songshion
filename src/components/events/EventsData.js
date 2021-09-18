eea17a27797b6c64a7fe6536a64d23fbed626f15
tejeshpatel <tejeshpatel@live.com>
2021-09-18 03:38:54 -0500

    finalized css edits for concert page

--- src/components/events/EventsData.js
+++ src/components/events/EventsData.js
@@ -1,4 +1,6 @@
-import { Card } from 'antd';
+import { Card, Col, Row } from 'antd';
+import { Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import React, { useState, useEffect } from 'react';
 import Moment from 'moment';
 // import ''
@@ -14,29 +16,29 @@ export default function EventData() {
   const [fetchDate, setFetchDate] = useState([])
   const [fetchVenue, setFetchVenue] = useState([])
   const [events, setEvents] = useState([]);
-  async function handleInput(e, value){
+  async function handleInput(e, value) {
     setSearch(e.target.value);
-    setFetchName({...fetchName, [e]: value})
+    setFetchName({ ...fetchName, [e]: value })
   };
   async function handleClick(e) {
     e.preventDefault();
     const updateEvents = [...events, fetchName]
     setEvents(updateEvents)
-    setFetchName({name:[]})
+    setFetchName({ name: [] })
     const date = Moment().format()
     const startDate = date.slice(0, 19)
-    const endDate = Number(date.slice(8, 10))+3
+    const endDate = Number(date.slice(8, 10)) + 3
     const firstHalf = date.slice(0, 8)
     const secondHalf = date.slice(10, 19)
     const API_KEY = 'aTlH3EnwmeBRZCg1uZj01xUefGqN8bGI'
-    console.log("hello" , secondHalf);
-    const realEndDate = firstHalf + endDate.toString()+secondHalf;
-    console.log("realEndDate" , realEndDate);
+    console.log("hello", secondHalf);
+    const realEndDate = firstHalf + endDate.toString() + secondHalf;
+    console.log("realEndDate", realEndDate);
     console.log(date)
-    console.log(Number(date.slice(8, 10))+3)
+    console.log(Number(date.slice(8, 10)) + 3)
     try {
       const response = await fetch(
-       // 2021-09-16T21:30:36-05:00
+        // 2021-09-16T21:30:36-05:00
         `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=${search}&localStartEndDateTime=${startDate},${realEndDate}&apikey=${API_KEY}`,
         {
           method: 'GET',
@@ -50,18 +52,18 @@ export default function EventData() {
       setFetchTime(fetchedQuery._embedded.events[0].dates.start.localTime);
       setFetchDate(fetchedQuery._embedded.events[0].dates.start.localDate);
       setFetchVenue(fetchedQuery._embedded.events[0]._embedded.venues[0].name);
-    // line below works in pulling in artist name
+      // line below works in pulling in artist name
       // console.log('Artist Name:', fetchedQuery._embedded.events[0].name);
-    // lines below are to try to store the artist name
-    //   setList(fetchedQuery._embedded.events[0].name);
-    //   console.log('testingsetArtist:', setList);
+      // lines below are to try to store the artist name
+      //   setList(fetchedQuery._embedded.events[0].name);
+      //   console.log('testingsetArtist:', setList);
     } catch (err) {
       console.log(err);
-    } 
+    }
   }
-  useEffect(() =>{
+  useEffect(() => {
     console.log('Data', fetchData)
-  },[
+  }, [
     fetchData,
     fetchName,
     fetchVenue,
@@ -72,62 +74,71 @@ export default function EventData() {
   ]
   );
   const eventsList = fetchData.length
-  ?fetchData.map(event => <div key={event.id}>
-        <Card
-          title={event.name}
-          style={{
-            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5)',
-            padding: '.5rem 1rem',
-            margin: '5rem',
-            height: 'auto',
-            width: '75%',
-            backgroundColor: '#00000012',
-            textAlign: 'center',
+    ? fetchData.map(event => <div key={event.id}>
+      <Card
+        title={event.name}
+        style={{
+          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5)',
+          padding: '.5rem 1rem',
+          margin: '5rem',
+          fontSize: "4rem",
+          fontWeight: 'bolder',
+          color: '#81E979',
+          height: 'auto',
+          width: '75%',
+          backgroundColor: '#595A4A',
+          textAlign: 'center'
           }}
-        >
-          <img
-            style={{ 
-              padding: '3ex',
-              width: '300px' 
-            }}
-            alt={event.name}
-            src={event.images[0].url}
-          />
-          <p class="genre">Genre: {event.classifications[0].genre.name}</p>
-          <p>{event.venue}</p>
+      >
+    <p class="cardsubdetails">Genre: {event.classifications[0].genre.name}</p>
 
-          <div
-            className="cardcontainer"
-            style={{ position: 'static', 
-            bottom: '0', 
-            display: 'inline-block' ,
-            backgroundColor: '#e28743' }}
-          >
-          </div>
-        </Card>
+      <img
+        style={{
+          padding: '1ex',
+          height: '500px',
+          width: 'auto'
+        }}
+        alt={event.name}
+        src={event.images[0].url}
+      />
+      <p class="cardsubdetails">Where: {event._embedded.venues[0].name}</p>
+      <p class="cardsubdetails">Date: {event.dates.start.localDate}</p>
+
+      <div
+        className="cardcontainer"
+        style={{
+          position: 'relative',
+          bottom: '0',
+          backgroundColor: '#e28743'
+        }}
+      >
+      </div>
+    </Card>
+
+    
     {/* <img src={event.images[0].url} alt={event.name}/>
     <p>{event.name}</p>
     <p>{event.classifications[0].genre.name}</p>
     <p>{event.venue}</p>
     <p>{event.dates.start.localDate}{event.dates.start.localTime}</p> */}
-    </div>)
+    </div >)
   : <p>No data</p>
   // const eventsList = events.map((fetchName, index) =>{
   //   <li key={index}>
   //     {fetchName.name}
   //   </li>
   // });
-//   let musicData = fetchName._embedded.events.map((event)=>{
-//    if (musicData.length == 0){
-//      return (<div></div>)
-//    } else {
-//    return(
-//      <div>
-//        <p>YO</p>
-//      </div>
-//    )
-//    }
-//  })
+  //   let musicData = fetchName._embedded.events.map((event)=>{
+  //    if (musicData.length == 0){
+  //      return (<div></div>)
+  //    } else {
+  //    return(
+  //      <div>
+  //        <p>YO</p>
+  //      </div>
+  //    )
+  //    }
+  //  })
   // const display = list.map(gif => {
   //   return (
   //     <img src= "" alt= "" type= "gif"></img>
@@ -135,14 +146,18 @@ export default function EventData() {
   // })
   return (
     <div className="App">
-      <h1> Events </h1>
+      <h1> Please enter your city </h1>
       <form>
-        <input
+        <input className="cityenter"
           type="text"
-          placeholder="search you City"
+          placeholder="Search your city"
           onChange={handleInput}
         />
-        <button onClick={handleClick}>Search</button>
+        {/* <button onClick={handleClick}>Search</button> */}
+          <Button type="primary" onClick={handleClick} icon={<SearchOutlined />}>
+          Search
+        </Button>
+
         {/* <input type="button" onClick={handleClick} /> */}
       </form>
       <div>
@@ -151,4 +166,4 @@ export default function EventData() {
       {/* {musicData} */}
     </div>
   );
-  }
+}
